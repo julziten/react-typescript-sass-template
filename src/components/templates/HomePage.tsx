a582e96ea42e3d814c415faa4d97f68dd685615b
Juliana Rocha <julianarocha@MMBA-B403.fibertel.com.ar>
2021-11-15 13:51:54 -0300

    Install Types

--- src/components/templates/HomePage.tsx
+++ src/components/templates/HomePage.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch
+  } from 'react-router-dom';
+  
 
 export const HomePage = () => {
     return (
         <div className='HomePage--Container'>
             <h1>Here is the HomePage</h1>
+
+            <div className='Navbar--Container'>
+                <ul>
+                    <li>
+                        <Link to="/"> Home </Link>
+                    </li>
+                    <li>
+                        <Link to="/about"> About </Link>
+                    </li>
+                    <li>
+                        <Link to="/users"> Users </Link>
+                    </li>
+                </ul>
+            </div>
+
+
+            <Switch>
+                <Route path="/about">
+                    <h1>About</h1>
+                </Route>
+            </Switch>
+
+
         </div>
     )
 }
