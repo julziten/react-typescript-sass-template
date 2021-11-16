a1afbc1a284f516c4390ae486c4ceff2705c901b
Juliana Rocha <julianarocha@MMBA-B403.local>
2021-11-16 06:45:40 -0300

    Types declaration added

--- src/components/routes/Invoice/Invoice.tsx
+++ src/components/routes/Invoice/Invoice.tsx
@@ -1,17 +1,20 @@
 import { useParams } from "react-router";
+import { getInvoice } from "../../../data";
 
 export const Invoice = () => {
     let params = useParams();
+    
     console.log('paramsHere', params.invoiceId);
-    // let invoice = getInvoice(parseInt(params.invoiceId, 10));
-    return <p>Check this then</p>
-    // return (
-    //     <main style={{ padding: '1rem' }}>
-    //         <h2>Total Due: {invoice.amount} </h2>
-    //         <p>
-    //             {invoice.name}: {invoice.number}
-    //         </p>
-    //         <p>Due Date: {invoice.due} </p>
-    //     </main>
-    // )
+
+    let invoice = getInvoice(parseInt(params.invoiceId as string, 10));
+
+    return (
+        <main style={{ padding: '1rem' }}>
+            <h2>Total Due: {invoice?.amount} </h2>
+            <p>
+                {invoice?.name}: {invoice?.number}
+            </p>
+            <p>Due Date: {invoice?.due} </p>
+        </main>
+    )
 }
